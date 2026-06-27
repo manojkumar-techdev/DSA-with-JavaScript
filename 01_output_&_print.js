4b82d8d7690cc818a482759b94e624db34c2a2f8
Manoj Kumar <contact@manojkumar-dev.com>
2026-06-27 20:26:10 +0000

    Code Corrected

--- 01_output_&_print.js
+++ 01_output_&_print.js
@@ -3,4 +3,13 @@ console.log("Hello World!");
 
 
 // print "I love JavaScript
-// console.log("I love JavaScript");"
+console.log("I love JavaScript");
+
+
+// add two numbers
+let num1 = 100;
+let num2 = 200;
+let sum = num1 + num2;
+console.log(sum);
+
+
