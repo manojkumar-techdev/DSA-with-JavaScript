e07b6e4566e36aa32f979a5ac46083dd86baac93
Manoj Kumar <contact@manojkumar-dev.com>
2026-06-29 23:41:18 +0200

    Update 02_variables.js

--- 02_variables.js
+++ 02_variables.js
@@ -8,3 +8,8 @@ console.log(age);
 // varibale -1 
 var number = 20;
 console.log(number - 1)
+
+// console.log multiple varia les in a single line
+var firstName = "Code";
+var lastName = "Chef";
+console.log(firstName, lastName);
