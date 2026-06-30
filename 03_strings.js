b1fb95dceddb63bd30b970fed3df7ce763754c0d
Manoj Kumar <contact@manojkumar-dev.com>
2026-06-30 21:48:30 +0000

    Update 03_strings.js

---
+++ 03_strings.js
@@ -0,0 +1,7 @@
+// strings
+var singleQuotedString = 'Hello, World!';
+var doubleQuotedString = "JavaScript is fun!";
+var backticksString = `You can use backticks too!`;
+console.log(singleQuotedString);
+console.log(doubleQuotedString);
+console.log(backticksString);
