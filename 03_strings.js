9ee3acb482dcdab393ef304475f88c0b17e4aa6d
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-01 22:50:00 +0200

    Update 03_strings.js

--- 03_strings.js
+++ 03_strings.js
@@ -5,3 +5,9 @@ var backticksString = `You can use backticks too!`;
 console.log(singleQuotedString);
 console.log(doubleQuotedString);
 console.log(backticksString);
+
+
+// concatination two strings
+var x = "Good";
+var y = "Work";
+console.log(x + y);   //output: GoodWork
