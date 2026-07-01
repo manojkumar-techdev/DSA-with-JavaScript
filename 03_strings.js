02380dfed1b521620e8feef1443aa2097142ec41
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-01 22:51:36 +0200

    Update 03_strings.js

--- 03_strings.js
+++ 03_strings.js
@@ -17,3 +17,10 @@ console.log(x + y);   //output: GoodWork
 var x = "Good";
 var y = "Work";
 console.log(x + " " + y);   // output: Good Work
+
+
+// string concatination
+var str1 = "Hello";
+var str2 = "World";
+var result = str1 + " " + str2;
+console.log(result); // Output: Hello World
